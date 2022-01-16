489cb1ec5d6cfdacf28e006faa2727a57143a566
Val <91499731+val-pivchenko@users.noreply.github.com>
2022-01-16 18:26:08 -0500

    e-commerce-app

---
+++ routes/admin/validators.js
@@ -0,0 +1,60 @@
+const { check, validationResult } = require('express-validator');
+const usersRepo = require('../../repositories/users');
+
+module.exports = {
+    requireTitle: check('title')
+        .trim()
+        .isLength({ min: 3, max: 40 })
+        .withMessage('Must be between 3 and 40 characters'),
+    requirePrice: check('price')
+        .trim()
+        .toFloat()
+        .isFloat({ min: 1 })
+        .withMessage('Must be a number greater than 1'),
+    requireEmail: check('email')
+        .trim()
+        .normalizeEmail()
+        .isEmail()
+        .withMessage('Must be a valid email')
+        .custom(async email => {
+            const existingUser = await usersRepo.getOneBy({ email });
+            if (existingUser) {
+                throw new Error('Email in use');
+            }
+        }),
+    requirePassword: check('password')
+        .trim()
+        .isLength({ min: 8, max: 20 })
+        .withMessage('Must be between 8 and 20 characters'),
+    requirePasswordConfirmation: check('passwordConfirmation')
+        .trim()
+        .custom(async (passwordConfirmation, { req }) => {
+            if (passwordConfirmation !== req.body.password) {
+                throw new Error('Passwords must match')
+            }
+        }),
+    requireEmailExists: check('email')
+        .isEmail()
+        .trim()
+        .normalizeEmail()
+        .withMessage('Must provode a valid Email')
+        .custom(async (email) => {
+            const user = await usersRepo.getOneBy({ email });
+            if (!user) {
+                throw new Error('Email not found');
+            }
+        }),
+    requireValidPassword: check('password')
+        .trim()
+        .custom(async (password, { req }) => {
+            const user = await usersRepo.getOneBy({ email: req.body.email });
+            if (!user) {
+                throw new Error('Invalid password');
+            }
+            const validPassword = await usersRepo.comparePasswords(user.password, password);
+
+            if (!validPassword) {
+                throw new Error('Invalid password');
+            }
+        })
+}
